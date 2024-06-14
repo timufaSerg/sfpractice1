9f7d4fc66747a18572381167d513295023f05442
timufaSerg <160013604+timufaSerg@users.noreply.github.com>
2024-06-14 15:10:47 +0500

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,3 @@
+<div class="footer">
+            <h2>We tried to learn the basics of PHP!</h2>
+        </div>
---
+++ index.php
@@ -0,0 +1,18 @@
+
+
+<?php 
+ $p = 'Welcome to my page!';
+?>
+
+<?php 
+ $name = 'Ivan';
+ $surname = 'Varlamov';
+ $city = 'Moscow';
+ $age = 45;
+?>
+
+
+<?php
+include 'main.php';
+?>
+
