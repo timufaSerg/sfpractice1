d911660327fcaa069f0622a7612b4af566f643ed
timufaSerg <sftimofeev@gmail.com>
2024-06-14 15:49:54 +0500

    delete all php
    del php

--- footer.inc.php
+++
@@ -1,3 +0,0 @@
-<div class="footer">
-            <h2>We tried to learn the basics of PHP!</h2>
-        </div>
--- menu.inc.php
+++
@@ -1,9 +0,0 @@
-            <nav>
-                <ul>
-                    <li><a href="#"> About my studies </a></li>
-                    <li><a href="#"> My portfolio </a></li>
-                    <li><a href="#"> My articles </a></li>
-                    <li><a href="#"> Links </a></li>
-                    <li><a href="#"> Contacts </a></li>
-                </ul>    
-            </nav>
